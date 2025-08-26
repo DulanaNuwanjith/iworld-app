4d080aed5b292d8344105f9d995bd8207d6547da
DulanaNuwanjith99 <dulana@hsenidlanka.com>
2025-08-26 19:31:18 +0530

    Initial commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+<div class="flex h-full w-full font-sans">
+    @include('layouts.side-bar')
+    <div class="flex-1 overflow-y-auto p-8 bg-gray-50 dark:bg-gray-900">
+        <!-- Header -->
+        <div class="flex justify-between items-center mb-8">
+            <h2 class="text-3xl font-bold text-gray-800 dark:text-white">Oder Management Dashboard</h2>
+        </div>
+
+
+    </div>
+</div>
